23f569e3e023f7dcf81c4dd38a0951a6898280ad
Kevanana26 <525kpiccinich@frhsd.com>
2024-01-11 10:18:25 -0500

    finished it

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-let adjective = prompt('Give me and Adjective, please and thank you')
-let noun = prompt('Give me and Noun, please and thank you')
-let verb = prompt('Give me and verb, please and thank you')
-let adverb = prompt('Give me and Adverb, please and thank you')
-
-let story = `Once upon a time, a ${adjective} ${noun}, a ${verb} ${noun} ${adverb} ${verb}`
+let holiday = prompt('Give me a Holiday, please and thank you')
+let place = prompt('Give me a place, please and thank you')
+let verb = prompt('Give me a Verb, please and thank you')
+let name = prompt('Give me a Name, please and thank you')
+let adjective2 = prompt('Give me another adjective, please and thank you')
+let activity = prompt('Give me and Activity, please and thank you')
+let person = prompt('Give me a Person, please and thank you')
+let verb2 = prompt('Give me another verb, please and thank you')
+let adjective = prompt('Give me an Adjective, please and thank you')
+let story = `Twas the night before ${holiday}, and everyone was ${adjective}, in the  ${place} except for ${name}, he was busy with ${activity} While he up he saw ${person} and he thought he was a ${adjective2} so he ${verb} until he left the house `
 
 let madlibOutputDiv = document.getElementById('madlibOutput')
 
